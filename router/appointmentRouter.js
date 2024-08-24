c7add9a5b052b953c66efe0affb0c6bc106c9932
Laxman Sakure <laxmansakure007@gmail.com>
2024-08-24 09:32:28 +0530

    auth change

--- router/appointmentRouter.js
+++ router/appointmentRouter.js
@@ -1,7 +1,8 @@
 import express from "express";
 import { postAppointment,getAllAppointments ,updateAppointmentStatus, deleteAppointment} from "../controller/appointmentContoller.js";
-  const router  = express.Router()
-router.post("/post", postAppointment)
+import {  isPatientAuthenticate} from "../middlewares/auth.js"
+ const router  = express.Router()
+router.post("/post", isPatientAuthenticate, postAppointment)
 router.get("/getall",   getAllAppointments)
 router.put("/update/:id", updateAppointmentStatus, getAllAppointments)
 router.delete("/delete/:id", deleteAppointment, getAllAppointments)

f6d5b407bf424f233579aef7de93f0d47e25f30a
Laxman Sakure <laxmansakure007@gmail.com>
2024-08-24 09:54:06 +0530

    login changes

--- utils/jwtTokens.js
+++ utils/jwtTokens.js
@@ -1,20 +1,21 @@
-import jwt from "jsonwebtoken"
+import jwt from 'jsonwebtoken';
 
 export const generateToken = (user, message, statusCode, res) => {
+  // Ensure the user is an instance of the User model with `generateWebToken` method
+  if (typeof user.generateWebToken !== 'function') {
+    throw new Error('User object does not have generateWebToken method');
+  }
+
   const token = user.generateWebToken();
-  const cookieName = user.role === "Admin" ? "adminToken" : "patientToken";
+  const cookieName = user.role === 'Admin' ? 'adminToken' : 'patientToken';
 
   res.status(statusCode).cookie(cookieName, token, {
     expires: new Date(Date.now() + process.env.COOKIE_EXPIRES * 24 * 60 * 60 * 1000),
-    httpOnly:true,
-
-   })
-  .json({
+    httpOnly: true,
+  }).json({
     success: true,
     message,
     user,
-    token
-
+    token,
   });
-
 };
